f8426355e322b033893f35de6bfef3953f044dc8
hygorp <hygor.henrique@live.com>
2017-01-24 11:59:28 -0300

    Update inputMasks.js

--- .gitignore/inputMasks.js
+++ .gitignore/inputMasks.js
@@ -1,19 +1,12 @@
 $(document).ready(function(){
 	$(function(){
-		$.mask.addPlaceholder("~","[+-]");
-		$("#cpf").mask("999.999.999-99");
-		$("#rg").mask("9.999.999");
-        $("#nascimento").mask("99/99/9999");
-		$("#cep").mask("99999-999");
-        $("#telefone").mask("(99) 9999-9999");
-        $("#celular").mask("(99) 99999-9999");
-        $("#comercial").mask("(99) 99999-9999");
-        $("#recado").mask("(99) 99999-9999");
-        $("#cnpj").mask("99.999.999/9999-99");
-        $("#ie").mask("999.999.999.999");
-        $("#validade").mask("99/99/9999");
-        $("#datacadastro").mask("99/99/9999");
-        $("#fabricacao").mask("99/99/9999");
-        $("#pesquisacpf").mask("999.999.999-99");
+	$.mask.addPlaceholder("~","[+-]");
+	$("#cpf").mask("999.999.999-99");
+	$("#rg").mask("9.999.999");
+        $("#data").mask("99/99/9999"); //exemplo para data
+	$("#cep").mask("99999-999"); //exemplo para cep
+        $("#telefone").mask("(99) 9999-9999"); //exemplo para telefone com DDD + 8 digitos
+        $("#celular").mask("(99) 99999-9999"); //exemplo para celular com DDD + 9 digitos
+        $("#cnpj").mask("99.999.999/9999-99"); //exemplo para cnpj
 	});
 });
